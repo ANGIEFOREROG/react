dcf4dbf071b3b3807330d692e2ea951156575537
ANGIEFOREROG <angieforero723@gmail.com>
2024-01-04 21:43:23 -0500

    modificaciones y correcciones la 2 preentrega

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//Componentes
+import MainRouter from './router/MainRouter';
+//Importacion de estilos bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Estilo
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='NavBarStyles'>
+        <MainRouter />
+      </div>
+  )
+}
+
+export default App
